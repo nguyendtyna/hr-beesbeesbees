219fa36806e288a22b308f6f323270736940b800
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 10:56:14 -0500

    Implement a Bee subclass to inherit properties from Grub

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
+/*
+
+Bee = Subclass of Grub
+Implement in pseudoclassical style
+
+*/
+
 var Bee = function() {
+  // Call the Grub superclass
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+// Inherit the prototype methods from the parent/superclass (Grub)
+Bee.prototype = Object.create(Grub.prototype);
+
+// Set the prototype constructor to the subclass (Bee)
+Bee.prototype.constructor = Bee;
