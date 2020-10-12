8c73e1ef52f927350bd866c5a84bb290b7c0b489
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 11:55:13 -0500

    Implement a ForagerBee subclass to inherit properties from Bee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,32 @@
+/*
+
+ForagerBee = Subclass of Bee (superclass of ForagerBee)
+Implement in pseudoclassical style
+
+*/
+
 var ForagerBee = function() {
+  // 'this' refers to Object.create(ForagerBee.prototype)
+  // Call the Bee superclass ('this' will now be bound to the new object)
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow';
+
+  // Add new ForagerBee properties
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// Inherit prototype methods from the superclass (Bee)
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+// Set the prototype constructor to the subclass (ForagerBee)
+ForagerBee.prototype.constructor = ForagerBee;
+
+// Add new methods
+ForagerBee.prototype.forage = function(treasure) {
+  // Add an input treasure to the treasureChest
+  this.treasureChest.push(treasure);
+}
