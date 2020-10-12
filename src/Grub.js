5da0c844f23ae943eb8a6243289ca77d9900de11
Tyna Nguyen <ngtyna@gmail.com>
2020-10-12 10:26:31 -0500

    Implement a Grub class in pseudoclassical style

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,23 @@
+/*
+
+Grub = Parent/superclass for all bees
+Implement in pseudoclassical style
+
+*/
+
 var Grub = function() {
+  // Generate an instance object
+
+  // Add properties to object
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
+  // Return instance object
+};
+
+// Add methods to instance object
+Grub.prototype.eat = function() {
+
 };
 
